5539033c591c7bb5454e07df3a2f5671738ba768
Mark Rendle <mark@markrendle.net>
2011-01-09 23:54:16 +0000

    Added conversions IObservable <-> Action

---
+++ OwinHelpers/BodyExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OwinHelpers
+{
+    public static class BodyExtensions
+    {
+        public static IObservable<T> ToObservable<T>(this Action<Action<T>, Action, Action<Exception>> source)
+        {
+            return new ActionObservable<T>(source);
+        }
+
+        private class ActionObservable<T> : IObservable<T>
+        {
+            private readonly Action<Action<T>, Action, Action<Exception>> _action;
+
+            public ActionObservable(Action<Action<T>, Action, Action<Exception>> action)
+            {
+                _action = action;
+            }
+
+            public IDisposable Subscribe(IObserver<T> observer)
+            {
+                _action.BeginInvoke(observer.OnNext, observer.OnCompleted, observer.OnError, _action.EndInvoke, null);
+                return new NullDisposable();
+            }
+        }
+    }
+}
---
+++ OwinHelpers/ObservableExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OwinHelpers
+{
+    public static class ObservableExtensions
+    {
+        public static Action<Action<T>, Action, Action<Exception>> ToAction<T>(this IObservable<T> source)
+        {
+            return
+                (onNext, onCompleted, onError) => source.Subscribe(new ActionObserver<T>(onNext, onCompleted, onError));
+        }
+
+        private class ActionObserver<T> : IObserver<T>
+        {
+            private readonly Action<T> _onNext;
+            private readonly Action _onCompleted;
+            private readonly Action<Exception> _onError;
+
+            public ActionObserver(Action<T> onNext, Action onCompleted, Action<Exception> onError)
+            {
+                _onNext = onNext ?? new Action<T>(x => { });
+                _onError = onError ?? new Action<Exception>(x => { });
+                _onCompleted = onCompleted ?? new Action(() => { });
+            }
+
+            public void OnNext(T value)
+            {
+                _onNext(value);
+            }
+
+            public void OnError(Exception error)
+            {
+                _onError(error);
+            }
+
+            public void OnCompleted()
+            {
+                _onCompleted();
+            }
+        }
+    }
+}
