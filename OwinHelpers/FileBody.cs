6aa0a606e8717e8c4746e6c68d7b7f6217a32748
Mark Rendle <mark@markrendle.net>
2011-01-09 22:13:34 +0000

    Added FileBody helper class.

---
+++ OwinHelpers/FileBody.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace OwinHelpers
+{
+    public class FileBody : IObservable<byte[]>
+    {
+        private readonly FileInfo _fileInfo;
+
+        public FileBody(FileInfo fileInfo)
+        {
+            _fileInfo = fileInfo;
+        }
+
+        public IDisposable Subscribe(IObserver<byte[]> observer)
+        {
+            try
+            {
+                using (var stream = _fileInfo.OpenRead())
+                {
+                    var buffer = new byte[stream.Length];
+                    stream.Read(buffer, 0, buffer.Length);
+                    observer.OnNext(buffer);
+                }
+                observer.OnCompleted();
+            }
+            catch (Exception ex)
+            {
+                observer.OnError(ex);
+            }
+
+            return new NullDisposable();
+        }
+    }
+}
