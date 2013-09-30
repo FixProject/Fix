7446c0de1dd66818184464bfaec7843e5b7ce795
Mark Rendle <mark@markrendle.net>
2013-10-01 00:47:12 +0100

    Added NuGet package for Fix.AppBuilder

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -4,10 +4,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Mark Rendle")]
 [assembly: AssemblyProduct("Fix")]
 [assembly: AssemblyCopyright("Copyright (C) Mark Rendle 2010-2013")]
-[assembly: AssemblyTrademark("938751bf48056ff2549cbc8f29a0e24a2c8a39ce")]
+[assembly: AssemblyTrademark("28779d381dcc746ac339e92e6e3eed69b358bdd7")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.4.0.51807")]
-[assembly: AssemblyFileVersion("0.4.0.51807")]
+[assembly: AssemblyVersion("0.4.0.50044")]
+[assembly: AssemblyFileVersion("0.4.0.50044")]
 
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("0.4.0.51807")]
+[assembly: AssemblyInformationalVersion("0.4.0.50044")]
+
+[assembly: InternalsVisibleTo("Tests")]
